a1b2e1e77a5798cd5253b2006c9c8afce6bf2866
Djentelman57 <105294978+Djentelman57@users.noreply.github.com>
2022-08-23 17:28:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,69 @@
+const hamburger = document.querySelector('.hamburger'),
+	menu = document.querySelector('.menu'),
+	closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+	menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+	menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+	lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+	lines[i].style.width = item.innerHTML;
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+	for (let anchor of anchors) {
+	  anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+		
+		const blockID = anchor.getAttribute('href').substr(1)
+		
+		document.getElementById(blockID).scrollIntoView({
+		  behavior: 'smooth',
+		  block: 'start'
+		})
+	  })
+	}
+});
+
+const animItems = document.querySelectorAll(`._anim-items`)
+if (animItems.length > 0) {
+    window.addEventListener(`scroll`, animOnScroll)
+
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index]
+            const animItemHeight = animItem.offsetHeight
+            const animItemOffSet = offset(animItem).top
+            const animStart = 4
+            let animItemPoint = window.innerHeight - animItemHeight / animStart
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart
+            }
+            if ((pageYOffset > animItemOffSet - animItemPoint) && pageYOffset < (animItemOffSet + animItemHeight)) {
+                animItem.classList.add(`_active`)
+            } else {
+                if (!(animItem.classList.contains(`_anim-no-hide`))) {
+                    animItem.classList.remove(`_active`)
+                }
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect()
+        let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+
+    setTimeout(() => {
+        animOnScroll()
+    }, 300)
+}
