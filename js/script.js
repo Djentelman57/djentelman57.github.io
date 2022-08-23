0e13e868156484de5cd291b60e996ce3515ddf69
Djentelman57 <105294978+Djentelman57@users.noreply.github.com>
2022-08-24 02:20:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -90,3 +90,37 @@ if (animItems.length > 0) {
 	});
 	return false;
 }); */
+
+
+const TOKEN = "5791021283:AAG4nWbKcxkxg0SoEMZQKmu0t8zYpumzTZo";
+const CHAT_ID = "-1001603746484";
+const URI_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
+const success = document.getElementById('success');
+
+document.getElementById('tg').addEventListener('submit', function(e) {
+	e.preventDefault();
+
+	let message = `<b>Заявка с сайта!</b>\n`;
+	message += `<b>Отправитель: </b> ${ this.name.value }\n`;
+	message += `<b>Почта: </b> ${ this.email.value }\n`;
+	message += `<b>Сообщение: </b> ${ this.text.value }`;
+
+	axios.post(URI_API, {
+		chat_id: CHAT_ID,
+		parse_mode: 'html',
+		text: message
+	})
+	.then((res) => {
+		this.name.value = "";
+		this.email.value = "";
+		this.text.value = "";
+		success.innerHTML = "сообщение отправлено!";
+		success.style.display = "block";
+	})
+	.catch((err) => {
+		console.warn(err);
+	})
+	.finally(() => {
+		console.log('Конец');
+	})
+})
